39865b737b9d12e6325af56ea283f20e828a188f
Chandra Shekhar pandey <cspandey3000@gmail.com>
2022-05-07 16:54:25 +0530

    debug

--- demo/src/main/java/utilities/genericUtility.java
+++ demo/src/main/java/utilities/genericUtility.java
@@ -3,32 +3,30 @@ package utilities;
 import java.util.Iterator;
 import java.util.List;
 
-import com.github.dockerjava.api.model.LogConfig.LoggingType;
-
 import enums.loggerType;
-import io.netty.handler.logging.LogLevel;
-import reports.frameworkLogger;
 
+import reports.frameworkLogger;
 
 public class genericUtility {
 
     public static <T> List<T> removeNullFromList(List<T> list) {
 
         Iterator<T> itr = list.iterator();
-       try {
-        while (itr.hasNext()) {
-            if (itr.next() == null || itr.next().toString().length()<=0) {
-                itr.remove();
+        try {
+            while (itr.hasNext()) {
+                if (itr.next() == null || itr.next().toString().length() <= 0) {
+                    String a= itr.next().toString();
+                    frameworkLogger.log(loggerType.EXTENTREPORTANDCONSOLE, a);
+                    itr.remove();
+                }
+
             }
 
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            frameworkLogger.log(loggerType.EXTENTREPORTANDCONSOLE, msg);
         }
-           
-       } catch (Exception e) {
-           String msg = e.getMessage();
-           frameworkLogger.log(loggerType.EXTENTREPORTANDCONSOLE, msg);
-       }
-        
-        
+
         return list;
 
     }
