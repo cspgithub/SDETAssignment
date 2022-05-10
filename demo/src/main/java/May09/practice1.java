6b914618c545b87b8f0efe43312e800dd75a329d
Chandra Shekhar pandey <cspandey3000@gmail.com>
2022-05-10 21:08:10 +0530

    addd

--- demo/src/main/java/May09/practice1.java
+++ demo/src/main/java/May09/practice1.java
@@ -1,22 +1,47 @@
 package May09;
 
 import java.util.ArrayList;
-
+import java.util.Arrays;
 import java.util.List;
+
+import org.checkerframework.checker.units.qual.A;
 
 public class practice1 {
 
     public static void main(String[] args) {
 
-        //final int[] array = { 5, 6, 8, 0, 34325, 324 };
+        // final int[] array = { 5, 6, 8, 0, 34325, 324 };
         List<Integer> listOfNumbers = new ArrayList<Integer>();
         listOfNumbers.add(4);
         listOfNumbers.add(53464);
         listOfNumbers.add(3434);
         listOfNumbers.add(90904);
-        System.out.println("list After sorting" + usingSort(listOfNumbers));
 
-        System.out.println("largerst number in array is :--" + largestInArray(convertListToArray(listOfNumbers)));
+        int[] array = convertListToArray(listOfNumbers);
+        System.out.println("second higest ---" + secondHighest(array));
+
+        /*
+         * System.out.println("list After sorting" + usingSort(listOfNumbers));
+         * 
+         * System.out.println("largerst number in array is :--" +
+         * largestInArray(convertListToArray(listOfNumbers)));
+         */
+    }
+
+    public static int[] convertListToArray(List<Integer> list) { 
+
+        int[] array = list.stream().mapToInt(i->i).toArray();//transforming each element of list primitive int
+       
+        return array;
+
+    }
+
+    public static int secondHighest(int[] array) {
+
+        Arrays.sort(array);
+        int total = array.length;
+        int secondHighest = array[total - 2];
+        return secondHighest;
 
     }
 
@@ -39,13 +64,6 @@ public class practice1 {
         listOfNumbers.sort((a, b) -> a.compareTo(b));
 
         return listOfNumbers;
-
-    }
-
-    public static int[] convertListToArray(List<Integer> list) {
-
-        int[] array = list.stream().mapToInt(i -> i).toArray();
-        return array;
 
     }
 
