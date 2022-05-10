d4da11dcebc84570c0bd3114718908c3af2a23a2
Chandra Shekhar pandey <cspandey3000@gmail.com>
2022-05-10 22:19:37 +0530

    created two testng.xml

--- demo/src/test/java/tests/test.java
+++ demo/src/test/java/tests/test.java
@@ -12,14 +12,14 @@ public class test extends baseTest {
   @Test
   public void verifyAttendance() {
 
-    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "HCL Attendsnce tescase");
+    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "1");
 
-    login login = new login();
+    /* login login = new login();
     String actualURL = login.loginisLoaded("chandrashekhar_pande@hcl.com", "Hcl@2022$");
     Assert.assertEquals("https://www.myhcl.com/bprhome/Home/Index", actualURL);
     login
         .markattendance("https://wf24.myhcl.com/TSMS/login.aspx");
-    login.checkPendingStatusAndSubmitHours("https://wf24.myhcl.com/TSMS/login.aspx"); 
+    login.checkPendingStatusAndSubmitHours("https://wf24.myhcl.com/TSMS/login.aspx");  */
 
   }
 
--- demo/src/test/java/tests/test2.java
+++ demo/src/test/java/tests/test2.java
@@ -5,33 +5,33 @@ import org.testng.annotations.Test;
 import enums.loggerType;
 import frameworkLogger.frameworkLog;
 
-public class test1 extends baseTest {
+public class test2 extends baseTest {
 
   @Test
   public void verifyAttendance1() {
 
-    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "2");
+    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "6");
 
   }
 
   @Test
   public void verifyAttendance4() {
 
-    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "3");
+    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "7");
 
   }
 
   @Test
   public void verifyAttendance2() {
 
-    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "4");
+    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "8");
 
   }
 
   @Test
   public void verifyAttendance3() {
 
-    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "5");
+    frameworkLog.log(loggerType.EXTENTREPORTANDCONSOLE, "9");
 
   }
 
